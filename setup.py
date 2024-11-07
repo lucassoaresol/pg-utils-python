803d4ec2169e0b35b7ae332af30cdfe93e992b7d
Lucas <lucas@insidedb.com.br>
2024-11-07 16:17:20 -0300

    chore(setup): bump version to 0.1.1 and reformat setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pg_utils",                      
-    version="0.1.0",                          
+    name="pg_utils",
+    version="0.1.1",
     description="A simple database utility library using psycopg3",
     author="Lucas Soares",
     author_email="lucassoaresolv@outlook.com",
-    url="https://github.com/lucassoaresol/pg-utils-python",  
-    packages=find_packages(),                
-    install_requires=[                      
+    url="https://github.com/lucassoaresol/pg-utils-python",
+    packages=find_packages(),
+    install_requires=[
         "psycopg[binary]>=3.0.0",
     ],
-    python_requires=">=3.7",                 
-    classifiers=[                           
+    python_requires=">=3.7",
+    classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
